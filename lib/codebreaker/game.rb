2a0f209f0bdc8b50c64a06183e295d09b671a080
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-10-14 13:13:59 -0500

    general refactoring

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -11,22 +11,41 @@ module Codebreaker
     end
 
     def guess(guess)
-      mark = ""
-      4.times do |index|
-        mark << "+" if exact_match?(guess, index)
-      end
-      4.times do |index|
-        mark << "-" if number_match?(guess, index)
-      end
-      mark = "0" if mark == ""
-      @message.puts mark
+      marker = Marker.new(@secret, guess)
+      result = '+'*marker.exact_match_count + '-'*marker.number_match_count
+      result = "0" if result == ""
+      @message.puts result
     end
 
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
+    class Marker
+      def initialize(secret, guess)
+        @secret = secret
+        @guess = guess
+      end
+
+      def exact_match?(index)
+        @guess[index] == @secret[index]
+      end
+
+      def number_match?(index)
+        @secret.include?(@guess[index]) && !exact_match?(index)
+      end
+
+      def exact_match_count
+        exact_match_count = 0
+        4.times.inject(0) do |count, index|
+          count + (exact_match?(index) ? 1 : 0)
+        end
+      end
+
+      def number_match_count
+        number_match_count = 0
+        4.times.inject(0) do |count, index|
+          count + (number_match?(index) ? 1 : 0)
+        end
+      end
+
     end
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
-    end
+
   end
 end
